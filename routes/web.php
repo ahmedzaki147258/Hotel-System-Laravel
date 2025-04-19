1778e2efc0e8da74eb59cc54556773178aeb197a
Fares Edres <faresedres1@gmail.com>
2025-04-19 14:06:35 +0200

    feat(Client Management): Implement client's router

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,11 @@ Route::get('/clients', [ClientController::class, 'index'])->name('clients.index'
 Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show');
 
 // Manager-only routes for editing, updating, and deleting clients
-Route::middleware('can:update,client')->group(function () {
-    Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-    Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
-    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
-});
+
+Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
+Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+
 
 
 require __DIR__ . '/settings.php';
