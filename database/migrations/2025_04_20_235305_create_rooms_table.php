ad03d2284cb44dd513408dbc9fa2474c7488e6e2
youssef-medhatt <youssefmedhatt2001@gmail.com>
2025-04-21 01:58:39 +0200

    add floor and room model and migration files

---
+++ database/migrations/2025_04_20_235305_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->unique();
+            $table->integer('capacity');
+            $table->string('status');
+            $table->bigInteger('price');
+            $table->foreignId('floor_id')->constrained('floors')->onDelete('restrict');
+            $table->foreignId('manager_id')->constrained('staff')->onDelete('restrict');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
