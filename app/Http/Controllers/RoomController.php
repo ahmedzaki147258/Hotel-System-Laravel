c514f8359c1ee613369b795c50e83232e92e34f3
youssef-medhatt <youssefmedhatt2001@gmail.com>
2025-04-23 00:42:59 +0200

    modified room contoller add store update edit and destroy methods

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -15,12 +15,16 @@ class RoomController extends Controller
      */ public function index()
     {      
         $rooms = Room::all();
+        $rooms = $rooms->map(function ($room) {
+            $room->floor_name = Floor::find($room->floor_id)->name; 
+            return $room;
+        });
          $currentStaff = Staff::find(auth()->id());
         // Check if the user has the 'manager' or 'admin' role
         if ($currentStaff->hasRole('admin')) {
             // Perform some transformations or mapping for admins
             $rooms = $rooms->map(function ($room) {
-                $room->manager_name = Staff::find($room->manager_id)->name; // Assuming the function we defined earlier
+                $room->manager_name = Staff::find($room->manager_id)->name; 
                 return $room;
             });
         }
@@ -50,9 +54,40 @@ class RoomController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        dd($request->all());
-    }
+{
+    $request->validate([
+        'capacity' => 'required|numeric|min:1|max:5', // Ensure capacity is at least 1
+        'price' => 'required|numeric|min:0.5', // Ensure price is a positive number
+        'manager_id' => 'required|exists:staff,id', // Ensure manager exists
+        'floor_id' => 'required|exists:floors,id', // Ensure floor exists
+    ]);
+    $floorId = $request->input('floor_id');
+
+    $rooms = Room::where('floor_id', $floorId)->get();
+        $maxRoomNumber = 0;
+        // Find the maximum room number for the selected floor
+        if($rooms->isEmpty()) {
+            $maxRoomNumber = Floor::find($floorId)->number; 
+        } else{
+            foreach ($rooms as $room) {
+                if ($room->number > $maxRoomNumber) {
+                    $maxRoomNumber = $room->number;
+                }
+            }
+        }
+        $nextRoomNumber = $maxRoomNumber + 1; 
+        Room::create([
+            'name' => $request->name,
+            'capacity' => $request->capacity,
+            'price' => $request->price*100, // Store price in cents
+            'manager_id' => $request->manager_id, // Store manager ID
+            'floor_id' => $floorId, // Store floor ID
+            'status' => 'available', // Set default status to available
+            'number' => $nextRoomNumber, // Store the next room number
+        ]);
+        return redirect()->route('rooms.index')->with('success', 'Room created successfully.');
+}
+
 
     /**
      * Display the specified resource.
@@ -67,7 +102,10 @@ class RoomController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return Inertia::render('Rooms/Edit', [
+            'room' => Room::findOrFail($id),
+            'floors' => Floor::all(),
+        ]);
     }
 
     /**
@@ -75,7 +113,16 @@ class RoomController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validatedData = $request->validate([
+            'capacity' => 'required|numeric|min:1|max:5', // Ensure capacity is at least 1
+            'price' => 'required|numeric|min:0.5', // Ensure price is a positive number
+            'floor_id' => 'required|exists:floors,id', // Ensure floor exists
+        ]);
+        $validatedData['price'] = $validatedData['price'] * 100; // Store price in cents
+        $room = Room::find($id);
+        $room->update($validatedData);
+
+        return redirect()->route('rooms.index')->with('success', 'Room updated successfully.');
     }
 
     /**
@@ -83,6 +130,8 @@ class RoomController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+       $room = Room::findOrFail($id);
+        $room->delete();
+        return redirect()->route('rooms.index')->with('success', 'Room deleted successfully.');
     }
 }
