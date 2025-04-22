e208acb042578caecb6e682e2442dbb8c923c14f
youssef-medhatt <youssefmedhatt2001@gmail.com>
2025-04-22 21:48:02 +0200

    "add fillable to floor model"

--- app/Models/Floor.php
+++ app/Models/Floor.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 class Floor extends Model
 {
+    protected $fillable = [
+        'name',
+        'number',
+        'manager_id', // Assuming you have a manager_id field in your floors table
+    ];
     public function rooms(): HasMany
     {
         return $this->hasMany(Room::class, 'floor_id', 'id');
