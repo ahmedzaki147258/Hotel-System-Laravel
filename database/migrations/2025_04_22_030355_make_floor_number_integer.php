9d5c10d9df35c279c9fc3bfd2a49bff336628896
youssef-medhatt <youssefmedhatt2001@gmail.com>
2025-04-22 21:48:46 +0200

    "add make floor number migration"

---
+++ database/migrations/2025_04_22_030355_make_floor_number_integer.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('floors', function (Blueprint $table) {
+            // Change the 'number' column type to integer
+            $table->integer('number')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('floors', function (Blueprint $table) {
+            //
+        });
+    }
+};
